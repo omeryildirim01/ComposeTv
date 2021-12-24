b8d00aa15e2f5b9b8a7f94ee57aea09755693041
Ömer YILDIRIM <yildirimomer01@gmail.com>
2021-12-24 21:41:22 +0300

    initial commit

---
+++ app/src/main/java/com/yildirimomer/composetv/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.yildirimomer.composetv
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.yildirimomer.composetv.ui.theme.ComposeTvTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeTvTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeTvTheme {
+        Greeting("Android")
+    }
+}
